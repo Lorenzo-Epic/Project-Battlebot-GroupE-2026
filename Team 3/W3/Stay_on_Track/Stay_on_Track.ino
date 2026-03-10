d407ab6d4840387433c5a71d4348f97a2489e79c
JP <jp.moro.machado@student.nhlstenden.com>
2026-03-10 22:43:17 +0100

    Added calibration to line sensors

--- Team 3/W3/Stay_on_Track/Stay_on_Track.ino
+++ Team 3/W3/Stay_on_Track/Stay_on_Track.ino
@@ -5,9 +5,12 @@ const int RIGHT_FORWARD_PIN = 11;
 const int RIGHT_BACKWARD_PIN = 9;
 
 // D1 = A0, D2 = A1, D3 = A2, D4 = A3, D5 = A4, D6 = A5, D7 = A6, D8 = A7
-const int numberOfSensors = 7;
-const int lineSensor[numberOfSensors] = {A1, A2, A3, A4, A5, A6, A7};
-int lineValues[numberOfSensors];
+const int numberOfSensors = 8;
+const int lineSensor[numberOfSensors] = {A0, A1, A2, A3, A4, A5, A6, A7};
+int lineValues[numberOfSensors]; // this keeps the sensor readings of the moment
+
+// sensor calibration
+int weights[numberOfSensors] = {-237, -233, -224, -257, -250, -266, -287, -303};
 
 // thresholds
 const int LIGHT_SENSOR_WHITE_THRESHOLD = 400;
@@ -16,121 +19,125 @@ const int LIGHT_SENSOR_BLACK_THRESHOLD = 600;
 // speed settings
 int baseSpeed = 120;
 int correctionSpeed = 30;
-int recoverySpeed = 150;
+int recovoerySpeed = 150; //only used of line is completely lost
 
-// last black detection
-bool lastBlackDetected = false;
-
+//last black line detection
+bool lastBlackDetected = false; // can also be last joel detected
 
 // setup
-void setup()
+
+void setup() 
 {
+
   pinMode(LEFT_FORWARD_PIN, OUTPUT);
   pinMode(LEFT_BACKWARD_PIN, OUTPUT);
   pinMode(RIGHT_FORWARD_PIN, OUTPUT);
   pinMode(RIGHT_BACKWARD_PIN, OUTPUT);
 
-  for (int i = 0; i < numberOfSensors; i++)
-    pinMode(lineSensor[i], INPUT);
+  for (int i = 0; i < numberOfSensors; i++) pinMode(lineSensor[i], INPUT);
 
   stopMotors();
 }
 
+// line sensor loop
 
-// loop
-void loop()
-{
+void loop() {
   defaultLineSensor();
-  delay(10);
+  delay(10); // delay for some sensor stability
 }
 
-
-// line sensor logic
-void defaultLineSensor()
+// calibration function (corrects raw sensor values)
+int applyCalibration(int raw, int sensorIndex)
 {
+  int v = raw + weights[sensorIndex];
+  return constrain(v,0,1023);
+}
+
+// line sensor logic void
+
+void defaultLineSensor() {
   int maxSensorValue = 0;
 
-  // read sensors
-  for (int i = 0; i < numberOfSensors; i++)
+  // Read reflection sensor values
+  for (int i = 0; i < numberOfSensors; i++) 
   {
-    lineValues[i] = analogRead(lineSensor[i]);
-    if (lineValues[i] > maxSensorValue)
-      maxSensorValue = lineValues[i];
+    int raw = analogRead(lineSensor[i]);
+    lineValues[i] = applyCalibration(raw,i); // apply calibration before using the value
+
+    if(lineValues[i] > maxSensorValue) maxSensorValue = lineValues[i];
   }
 
-  // robot sees black line
-  if (maxSensorValue >= LIGHT_SENSOR_BLACK_THRESHOLD)
+  // this part decides movement
+  if(maxSensorValue >= LIGHT_SENSOR_BLACK_THRESHOLD) 
   {
-    lastBlackDetected = true;
+    lastBlackDetected = true; // remembers last time black line was seen
 
-    // center sensor > forward
-    if (lineValues[3] >= LIGHT_SENSOR_BLACK_THRESHOLD || lineValues[4] >= LIGHT_SENSOR_BLACK_THRESHOLD || lineValues[5] >= LIGHT_SENSOR_BLACK_THRESHOLD)
+    // so center sensors see line then move forward
+    if(lineValues[3] >= LIGHT_SENSOR_BLACK_THRESHOLD || lineValues[4] >= LIGHT_SENSOR_BLACK_THRESHOLD) 
     {
       driveForward(baseSpeed + correctionSpeed, baseSpeed + correctionSpeed);
     }
 
-    // right sensors > turn right
-    else if (lineValues[1] >= LIGHT_SENSOR_BLACK_THRESHOLD || lineValues[2] >= LIGHT_SENSOR_BLACK_THRESHOLD)
+    // if right sensors see line then turn right
+    else if(lineValues[1] >= LIGHT_SENSOR_BLACK_THRESHOLD || lineValues[2] >= LIGHT_SENSOR_BLACK_THRESHOLD || lineValues[3] >= LIGHT_SENSOR_BLACK_THRESHOLD) 
     {
       driveRight(baseSpeed + 40, baseSpeed + 50);
     }
 
-    // left sensors > turn left
-    else if (lineValues[6] >= LIGHT_SENSOR_BLACK_THRESHOLD || lineValues[7] >= LIGHT_SENSOR_BLACK_THRESHOLD)
+    // of left sensors see line then turn left
+    else if(lineValues[5] >= LIGHT_SENSOR_BLACK_THRESHOLD || lineValues[6] >= LIGHT_SENSOR_BLACK_THRESHOLD || lineValues[7] >= LIGHT_SENSOR_BLACK_THRESHOLD) 
     {
       driveLeft(baseSpeed + 50, baseSpeed + 40);
     }
+
+    else 
+    {
+      stopMotors();
+    } 
   }
 
-  // line completely lost (all white)
-  else if (maxSensorValue <= LIGHT_SENSOR_WHITE_THRESHOLD && lastBlackDetected)
+  // recovery logic when all sensors see white
+  else if(maxSensorValue <= LIGHT_SENSOR_WHITE_THRESHOLD && lastBlackDetected)
   {
-    driveBackward(recoverySpeed, recoverySpeed);
+    driveBackward(recovoerySpeed, recovoerySpeed); // robot reverses until it finds the line again
   }
 
-  // uncertain area
-  else
+  else 
   {
     stopMotors();
   }
 }
 
-
 // motor control
-void driveForward(int left, int right)
-{
+void driveForward(int left, int right) {
   analogWrite(LEFT_FORWARD_PIN, left);
   analogWrite(LEFT_BACKWARD_PIN, 0);
   analogWrite(RIGHT_FORWARD_PIN, right);
   analogWrite(RIGHT_BACKWARD_PIN, 0);
 }
 
-void driveBackward(int left, int right)
-{
+void driveBackward(int left, int right) {
   analogWrite(LEFT_FORWARD_PIN, 0);
   analogWrite(LEFT_BACKWARD_PIN, left);
   analogWrite(RIGHT_FORWARD_PIN, 0);
   analogWrite(RIGHT_BACKWARD_PIN, right);
 }
 
-void driveRight(int left, int right)
-{
+void driveRight(int left, int right) {
   analogWrite(LEFT_FORWARD_PIN, left);
   analogWrite(LEFT_BACKWARD_PIN, 0);
   analogWrite(RIGHT_FORWARD_PIN, 0);
   analogWrite(RIGHT_BACKWARD_PIN, right);
 }
 
-void driveLeft(int left, int right)
-{
+void driveLeft(int left, int right) {
   analogWrite(LEFT_FORWARD_PIN, 0);
   analogWrite(LEFT_BACKWARD_PIN, left);
   analogWrite(RIGHT_FORWARD_PIN, right);
   analogWrite(RIGHT_BACKWARD_PIN, 0);
 }
 
-void stopMotors()
-{
+void stopMotors() {
   analogWrite(LEFT_FORWARD_PIN, 0);
   analogWrite(LEFT_BACKWARD_PIN, 0);
   analogWrite(RIGHT_FORWARD_PIN, 0);
