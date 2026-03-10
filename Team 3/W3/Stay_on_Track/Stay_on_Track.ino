9ebcc0a6d37f591978de85b1329c8e45ee0ebe99
JP <jp.moro.machado@student.nhlstenden.com>
2026-03-10 15:48:18 +0100

    Line follower code

--- Team 3/W3/Stay_on_Track/Stay_on_Track.ino
+++ Team 3/W3/Stay_on_Track/Stay_on_Track.ino
@@ -1,129 +1,138 @@
-// this is a WIP will work on it more later.
-
 // pin mapping
-const int LEFT_BACKWARD_PIN  = 10;
-const int LEFT_FORWARD_PIN   = 5;
-const int RIGHT_FORWARD_PIN  = 6;
+const int LEFT_FORWARD_PIN  = 7;
+const int LEFT_BACKWARD_PIN = 10;
+const int RIGHT_FORWARD_PIN = 11;
 const int RIGHT_BACKWARD_PIN = 9;
 
-const int NUM_SENSORS = 8;
-const int LIGHT_SENSOR_PINS[NUM_SENSORS] = {A0, A1, A2, A3, A4, A5, A6, A7};
+// D1 = A0, D2 = A1, D3 = A2, D4 = A3, D5 = A4, D6 = A5, D7 = A6, D8 = A7
+const int numberOfSensors = 7;
+const int lineSensor[numberOfSensors] = {A1, A2, A3, A4, A5, A6, A7};
+int lineValues[numberOfSensors];
 
 // thresholds
 const int LIGHT_SENSOR_WHITE_THRESHOLD = 400;
 const int LIGHT_SENSOR_BLACK_THRESHOLD = 600;
 
-// global variable
-int previousError = 0;  // used in readLinePosition() to continue last direction
+// speed settings
+int baseSpeed = 120;
+int correctionSpeed = 30;
+int recoverySpeed = 150;
 
-void setup() {
-  Serial.begin(9600);
+// last black detection
+bool lastBlackDetected = false;
 
+
+// setup
+void setup()
+{
   pinMode(LEFT_FORWARD_PIN, OUTPUT);
   pinMode(LEFT_BACKWARD_PIN, OUTPUT);
   pinMode(RIGHT_FORWARD_PIN, OUTPUT);
   pinMode(RIGHT_BACKWARD_PIN, OUTPUT);
 
-  for (int i = 0; i < NUM_SENSORS; i++)
-    pinMode(LIGHT_SENSOR_PINS[i], INPUT);
+  for (int i = 0; i < numberOfSensors; i++)
+    pinMode(lineSensor[i], INPUT);
 
-  driveMotors(0, 0); // stop motors
+  stopMotors();
 }
 
-void loop() {
 
-  int s3 = analogRead(LIGHT_SENSOR_PINS[3]);
-  int s4 = analogRead(LIGHT_SENSOR_PINS[4]);
-
-  if(s3 > LIGHT_SENSOR_BLACK_THRESHOLD && s4 > LIGHT_SENSOR_BLACK_THRESHOLD){
-      driveMotors(255,255);   // straight
-  }
-  else if(s3 > LIGHT_SENSOR_BLACK_THRESHOLD){
-      driveMotors(150,255);   // slight left
-  }
-  else if(s4 > LIGHT_SENSOR_BLACK_THRESHOLD){
-      driveMotors(255,150);   // slight right
-  }
-  else{
-      driveMotors(150,-150);  // search
-  }
+// loop
+void loop()
+{
+  defaultLineSensor();
+  delay(10);
 }
 
-int baseSpeed = 255;   // reduced for stability
-const int SPEED_CENTER = 255;   // A3/A4
-const int SPEED_OUTER  = 200;   // A2/A5
-const int SPEED_EDGE   = 150;   // A0/A1/A6/A7
 
-int sensorWeights[NUM_SENSORS] = {-4000,-2500,-500,-500,500,500,2500,4000};
-/// original value: {-3500,-2500,-1500,-500,500,1500,2500,3500}
+// line sensor logic
+void defaultLineSensor()
+{
+  int maxSensorValue = 0;
 
-// sensor calibration
-int weights[NUM_SENSORS] = {-237, -233, -224, -257, -250, -266, -287, -303};
+  // read sensors
+  for (int i = 0; i < numberOfSensors; i++)
+  {
+    lineValues[i] = analogRead(lineSensor[i]);
+    if (lineValues[i] > maxSensorValue)
+      maxSensorValue = lineValues[i];
+  }
 
+  // robot sees black line
+  if (maxSensorValue >= LIGHT_SENSOR_BLACK_THRESHOLD)
+  {
+    lastBlackDetected = true;
 
-// apply calibration
-int applyLightSensorCalibration(int raw, int sensorIndex) {
-  int v = raw + weights[sensorIndex];
-  return constrain(v, 0, 1023);
-}
+    // center sensor > forward
+    if (lineValues[3] >= LIGHT_SENSOR_BLACK_THRESHOLD || lineValues[4] >= LIGHT_SENSOR_BLACK_THRESHOLD || lineValues[5] >= LIGHT_SENSOR_BLACK_THRESHOLD)
+    {
+      driveForward(baseSpeed + correctionSpeed, baseSpeed + correctionSpeed);
+    }
 
-// read line position (A3/A4 using others for recovery)
-int readLinePosition() {
-  long weightedSum = 0;
-  int activeCount = 0;
+    // right sensors > turn right
+    else if (lineValues[1] >= LIGHT_SENSOR_BLACK_THRESHOLD || lineValues[2] >= LIGHT_SENSOR_BLACK_THRESHOLD)
+    {
+      driveRight(baseSpeed + 40, baseSpeed + 50);
+    }
 
-  // main sensors
-  for (int i = 3; i <= 4; i++) {  // A3 and A4
-    int raw = analogRead(LIGHT_SENSOR_PINS[i]);
-    int calibrated = applyLightSensorCalibration(raw, i);
-
-    if (calibrated > LIGHT_SENSOR_BLACK_THRESHOLD) {  // black is higher
-      weightedSum += sensorWeights[i];
-      activeCount++;
+    // left sensors > turn left
+    else if (lineValues[6] >= LIGHT_SENSOR_BLACK_THRESHOLD || lineValues[7] >= LIGHT_SENSOR_BLACK_THRESHOLD)
+    {
+      driveLeft(baseSpeed + 50, baseSpeed + 40);
     }
   }
 
-  // recovery sensors
-  if (activeCount == 0) {
-    for (int i = 0; i < NUM_SENSORS; i++) {
-      if (i >= 3 && i <= 4) continue;  // skip center again
-
-      int raw = analogRead(LIGHT_SENSOR_PINS[i]);
-      int calibrated = applyLightSensorCalibration(raw, i);
-
-      if (calibrated > LIGHT_SENSOR_BLACK_THRESHOLD) {
-        weightedSum += sensorWeights[i];
-        activeCount++;
-      }
-    }
+  // line completely lost (all white)
+  else if (maxSensorValue <= LIGHT_SENSOR_WHITE_THRESHOLD && lastBlackDetected)
+  {
+    driveBackward(recoverySpeed, recoverySpeed);
   }
 
-  // if loses line
-  if (activeCount == 0) {
-    return previousError / 2;  // gently continue in last direction
+  // uncertain area
+  else
+  {
+    stopMotors();
   }
-
-  return weightedSum / activeCount;
 }
+
 
 // motor control
-void driveMotors(int left, int right) {
-  left  = constrain(left, -255, 255);
-  right = constrain(right, -255, 255);
+void driveForward(int left, int right)
+{
+  analogWrite(LEFT_FORWARD_PIN, left);
+  analogWrite(LEFT_BACKWARD_PIN, 0);
+  analogWrite(RIGHT_FORWARD_PIN, right);
+  analogWrite(RIGHT_BACKWARD_PIN, 0);
+}
 
-  if (left >= 0) {
-    analogWrite(LEFT_FORWARD_PIN, left);
-    analogWrite(LEFT_BACKWARD_PIN, 0);
-  } else {
-    analogWrite(LEFT_FORWARD_PIN, 0);
-    analogWrite(LEFT_BACKWARD_PIN, -left);
-  }
+void driveBackward(int left, int right)
+{
+  analogWrite(LEFT_FORWARD_PIN, 0);
+  analogWrite(LEFT_BACKWARD_PIN, left);
+  analogWrite(RIGHT_FORWARD_PIN, 0);
+  analogWrite(RIGHT_BACKWARD_PIN, right);
+}
 
-  if (right >= 0) {
-    analogWrite(RIGHT_FORWARD_PIN, right);
-    analogWrite(RIGHT_BACKWARD_PIN, 0);
-  } else {
-    analogWrite(RIGHT_FORWARD_PIN, 0);
-    analogWrite(RIGHT_BACKWARD_PIN, -right);
-  }
+void driveRight(int left, int right)
+{
+  analogWrite(LEFT_FORWARD_PIN, left);
+  analogWrite(LEFT_BACKWARD_PIN, 0);
+  analogWrite(RIGHT_FORWARD_PIN, 0);
+  analogWrite(RIGHT_BACKWARD_PIN, right);
+}
+
+void driveLeft(int left, int right)
+{
+  analogWrite(LEFT_FORWARD_PIN, 0);
+  analogWrite(LEFT_BACKWARD_PIN, left);
+  analogWrite(RIGHT_FORWARD_PIN, right);
+  analogWrite(RIGHT_BACKWARD_PIN, 0);
+}
+
+void stopMotors()
+{
+  analogWrite(LEFT_FORWARD_PIN, 0);
+  analogWrite(LEFT_BACKWARD_PIN, 0);
+  analogWrite(RIGHT_FORWARD_PIN, 0);
+  analogWrite(RIGHT_BACKWARD_PIN, 0);
 }
