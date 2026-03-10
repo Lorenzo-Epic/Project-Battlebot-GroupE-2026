86e1964f274040840b2f3d1afd7bbfa51d8f4643
JP <jp.moro.machado@student.nhlstenden.com>
2026-03-10 23:19:58 +0100

    Updated maze solver follows the left wall always

--- Team 3/W5/Maze_logic_V1/Maze_logic_V1/Maze_logic_V1.ino
+++ Team 3/W5/Maze_logic_V1/Maze_logic_V1/Maze_logic_V1.ino
@@ -1,317 +1,156 @@
-#include <string.h> // c library to use strcmp
+// pins 
+const int LEFT_FORWARD_PIN  = 7;
+const int LEFT_BACKWARD_PIN = 10;
+const int RIGHT_FORWARD_PIN = 11;
+const int RIGHT_BACKWARD_PIN = 9;
 
-// setup runs when arduino starts initializing the hardware
+int motorSpeed = 150;
 
-void setup(){
 
-  Serial.begin(9600); // starts communication w computer its used for debugging
+// ultrasonic headhog
+//sensors r prob wrong cause i dont have the pins at home
+// front
+const int TRIG_FRONT = 2;
+const int ECHO_FRONT = 3;
+// leftie
+const int TRIG_LEFT = 4;
+const int ECHO_LEFT = 5;
+// right
+const int TRIG_RIGHT = 6;
+const int ECHO_RIGHT = 8;
 
-  pinMode(LEFT_FORWARD_PIN,OUTPUT);
-  pinMode(LEFT_BACKWARD_PIN,OUTPUT);
-  pinMode(RIGHT_FORWARD_PIN,OUTPUT);
-  pinMode(RIGHT_BACKWARD_PIN,OUTPUT);
-// ultrasonic sensor front
-  pinMode(FRONT_TRIG,OUTPUT); //send sound pulse
-  pinMode(FRONT_ECHO,INPUT); // receive reflectiong
-// sensor left
-  pinMode(LEFT_TRIG,OUTPUT);
-  pinMode(LEFT_ECHO,INPUT);
-// sensor right
-  pinMode(RIGHT_TRIG,OUTPUT);
-  pinMode(RIGHT_ECHO,INPUT);
-// encoder setup
-  pinMode(LEFT_ENCODER_PIN,INPUT);
-  pinMode(RIGHT_ENCODER_PIN,INPUT);
-// interrupts when encoder signal changes run isrLeft or isrRight immediately
-  attachInterrupt(digitalPinToInterrupt(LEFT_ENCODER_PIN),isrLeft,CHANGE);
-  attachInterrupt(digitalPinToInterrupt(RIGHT_ENCODER_PIN),isrRight,CHANGE);
-}
+// distance to wall
+const int WALL_DISTANCE = 20; // cm
 
-// loop
-// runs forever robot always repeats
-void loop(){
-//robot moves 60cm forward
-  move(CELL_DISTANCE_CM,"forward");
+// setup
 
-  if(reachedBlackZone()){
-    stopMotors();
-    while(true);
-  }
+void setup() {
 
-  int front = readDistance(FRONT_TRIG,FRONT_ECHO);
-  int left  = readDistance(LEFT_TRIG,LEFT_ECHO);
-  int right = readDistance(RIGHT_TRIG,RIGHT_ECHO);
+  Serial.begin(9600);
 
-  Serial.print("F:");
-  Serial.print(front);
-  Serial.print(" L:");
-  Serial.print(left);
-  Serial.print(" R:");
-  Serial.println(right);
-// maze decisions
-  if(left > LEFT_OPEN_DIST){
+  pinMode(LEFT_FORWARD_PIN, OUTPUT);
+  pinMode(LEFT_BACKWARD_PIN, OUTPUT);
+  pinMode(RIGHT_FORWARD_PIN, OUTPUT);
+  pinMode(RIGHT_BACKWARD_PIN, OUTPUT);
 
-    move(90,"left");
+  pinMode(TRIG_FRONT, OUTPUT);
+  pinMode(ECHO_FRONT, INPUT);
 
-  }
-  else if(front > FRONT_OPEN_DIST){
+  pinMode(TRIG_LEFT, OUTPUT);
+  pinMode(ECHO_LEFT, INPUT);
 
-    // continue forward
-
-  }
-  else{
-
-    move(90,"right");
-
-  }
-}
-
-// pins
-#define LEFT_FORWARD_PIN 5 // left wheel forward
-#define LEFT_BACKWARD_PIN 10 // left wheel backward
-#define RIGHT_FORWARD_PIN 6 // right wheel forward
-#define RIGHT_BACKWARD_PIN 9 // right wheel backward
-
-#define LEFT_ENCODER_PIN 2
-#define RIGHT_ENCODER_PIN 3
-
-#define FRONT_TRIG 4
-#define FRONT_ECHO 7
-#define LEFT_TRIG 12
-#define LEFT_ECHO 13
-#define RIGHT_TRIG A0
-#define RIGHT_ECHO A1
-
-#define SERVO_PIN 11
-
-// line sensors
-const int LINE_SENSOR_PINS[] = {A3,A4,A5,A6};
-#define BLACK_LINE_THRESHOLD 800
-
-// motor calibration
-const int CALIBRATION_FORWARD_LEFT = 255;
-const int CALIBRATION_BACKWARD_LEFT = 255;
-const int CALIBRATION_FORWARD_RIGHT = 243;
-const int CALIBRATION_BACKWARD_RIGHT = 210;
-
-// encoder geometry
-const float WHEEL_DIAMETER_CM = 6.5f;
-const int SLOTS_PER_REV = 20;
-const int EDGES_PER_SLOT = 2;
-const int TICKS_PER_REV = SLOTS_PER_REV * EDGES_PER_SLOT;
-
-const float CIRCUMFERENCE_CM = PI * WHEEL_DIAMETER_CM;
-const float TICKS_PER_CM = (float)TICKS_PER_REV / CIRCUMFERENCE_CM;
-
-// turn calibration
-const float TURN_SLOTS_FOR_90_DEG = 8.0f;
-const float TURN_TICKS_PER_DEG = (TURN_SLOTS_FOR_90_DEG * EDGES_PER_SLOT) / 90.0f;
-
-const long TURN_SLOWDOWN_TICKS = 3;
-
-const int TURN_SLOW_LEFT_FORWARD = 150;
-const int TURN_SLOW_LEFT_BACKWARD = 150;
-const int TURN_SLOW_RIGHT_FORWARD = 145;
-const int TURN_SLOW_RIGHT_BACKWARD = 135;
-
-// sensor limits
-#define FRONT_OPEN_DIST 15
-#define LEFT_OPEN_DIST 15
-#define RIGHT_OPEN_DIST 15
-
-#define CELL_DISTANCE_CM 60
-
-const unsigned long EDGE_MIN_US = 150;
-const unsigned long ULTRASOUND_TIMEOUT_US = 25000UL;
-
-// encoder globals
-volatile unsigned long g_leftTicks = 0;
-volatile unsigned long g_rightTicks = 0;
-volatile unsigned long g_lastLeftUs = 0;
-volatile unsigned long g_lastRightUs = 0;
-
-// encoder isr
-
-void isrLeft(){
-  unsigned long now = micros();
-  if(now - g_lastLeftUs >= EDGE_MIN_US){
-    g_leftTicks++;
-    g_lastLeftUs = now;
-  }
-}
-
-void isrRight(){
-  unsigned long now = micros();
-  if(now - g_lastRightUs >= EDGE_MIN_US){
-    g_rightTicks++;
-    g_lastRightUs = now;
-  }
-}
-
-// motor control
-
-void stopMotors(){
-
-  analogWrite(LEFT_FORWARD_PIN,0);
-  analogWrite(LEFT_BACKWARD_PIN,0);
-  analogWrite(RIGHT_FORWARD_PIN,0);
-  analogWrite(RIGHT_BACKWARD_PIN,0);
-
-  digitalWrite(LEFT_FORWARD_PIN,LOW);
-  digitalWrite(LEFT_BACKWARD_PIN,LOW);
-  digitalWrite(RIGHT_FORWARD_PIN,LOW);
-  digitalWrite(RIGHT_BACKWARD_PIN,LOW);
-}
-
-void driveForward(){
-
-  analogWrite(LEFT_BACKWARD_PIN,0);
-  analogWrite(RIGHT_BACKWARD_PIN,0);
-
-  analogWrite(LEFT_FORWARD_PIN,CALIBRATION_FORWARD_LEFT);
-  analogWrite(RIGHT_FORWARD_PIN,CALIBRATION_FORWARD_RIGHT);
-}
-
-void driveBackward(){
-
-  analogWrite(LEFT_FORWARD_PIN,0);
-  analogWrite(RIGHT_FORWARD_PIN,0);
-
-  analogWrite(LEFT_BACKWARD_PIN,CALIBRATION_BACKWARD_LEFT);
-  analogWrite(RIGHT_BACKWARD_PIN,CALIBRATION_BACKWARD_RIGHT);
-}
-
-void turnLeftInPlace(){
-
-  analogWrite(LEFT_FORWARD_PIN,0);
-  analogWrite(RIGHT_BACKWARD_PIN,0);
-
-  analogWrite(LEFT_BACKWARD_PIN,CALIBRATION_BACKWARD_LEFT);
-  analogWrite(RIGHT_FORWARD_PIN,CALIBRATION_FORWARD_RIGHT);
-}
-
-void turnRightInPlace(){
-
-  analogWrite(LEFT_BACKWARD_PIN,0);
-  analogWrite(RIGHT_FORWARD_PIN,0);
-
-  analogWrite(LEFT_FORWARD_PIN,CALIBRATION_FORWARD_LEFT);
-  analogWrite(RIGHT_BACKWARD_PIN,CALIBRATION_BACKWARD_RIGHT);
-}
-
-// encoder helpers (
-
-void readTicks(unsigned long &l,unsigned long &r){
-  noInterrupts();
-  l = g_leftTicks;
-  r = g_rightTicks;
-  interrupts();
-}
-
-void resetTicks(){
-  noInterrupts();
-  g_leftTicks = 0;
-  g_rightTicks = 0;
-  g_lastLeftUs = 0;
-  g_lastRightUs = 0;
-  interrupts();
-}
-
-static inline long roundToLong(float x){
-  return (x>=0.0f)?(long)(x+0.5f):(long)(x-0.5f);
-}
-
-// move function
-
-void move(float amount,const char *direction){
-
-  bool IS_DRIVE =
-  strcmp(direction,"forward")==0 || //strcmp(string1, string2) to commpare direction with forward
-  strcmp(direction,"backward")==0;
-
-  bool IS_TURN =
-  strcmp(direction,"left")==0 ||
-  strcmp(direction,"right")==0;
-
-  if(!IS_DRIVE && !IS_TURN) return;
-
-  float ABS_AMOUNT = abs(amount);
-
-  long targetTicks;
-
-  if(IS_DRIVE)
-    targetTicks = roundToLong(ABS_AMOUNT * TICKS_PER_CM);
-  else
-    targetTicks = roundToLong(ABS_AMOUNT * TURN_TICKS_PER_DEG);
-
-  resetTicks();
-
-  if(strcmp(direction,"forward")==0) driveForward();
-  else if(strcmp(direction,"backward")==0) driveBackward();
-  else if(strcmp(direction,"left")==0) turnLeftInPlace();
-  else if(strcmp(direction,"right")==0) turnRightInPlace();
-
-  unsigned long START_MS = millis();
-
-  unsigned long TIMEOUT_MS =
-  IS_TURN ? (700 + targetTicks*80)
-          : (1500 + targetTicks*120);
-
-  bool slowPhase = false;
-
-  while(true){
-
-    unsigned long l,r;
-    readTicks(l,r);
-
-    unsigned long progress = (l+r)/2;
-    long remaining = targetTicks - progress;
-
-    if(IS_TURN && !slowPhase && remaining <= TURN_SLOWDOWN_TICKS){
-
-      slowPhase = true;
-
-      if(strcmp(direction,"left")==0){
-
-        analogWrite(LEFT_FORWARD_PIN,0);
-        analogWrite(RIGHT_BACKWARD_PIN,0);
-        analogWrite(LEFT_BACKWARD_PIN,TURN_SLOW_LEFT_BACKWARD);
-        analogWrite(RIGHT_FORWARD_PIN,TURN_SLOW_RIGHT_FORWARD);
-
-      }else{
-
-        analogWrite(LEFT_BACKWARD_PIN,0);
-        analogWrite(RIGHT_FORWARD_PIN,0);
-        analogWrite(LEFT_FORWARD_PIN,TURN_SLOW_LEFT_FORWARD);
-        analogWrite(RIGHT_BACKWARD_PIN,TURN_SLOW_RIGHT_BACKWARD);
-      }
-    }
-
-    if(progress >= targetTicks) break;
-    if(millis()-START_MS > TIMEOUT_MS) break;
-  }
+  pinMode(TRIG_RIGHT, OUTPUT);
+  pinMode(ECHO_RIGHT, INPUT);
 
   stopMotors();
 }
 
-// ultrasonic
+// main loop
 
-long readDistance(int trig,int echo){
+void loop() {
 
-  digitalWrite(trig,LOW);
-  delayMicroseconds(2);
+  float frontDistance = getDistance(TRIG_FRONT, ECHO_FRONT);
+  float leftDistance  = getDistance(TRIG_LEFT, ECHO_LEFT);
+  float rightDistance = getDistance(TRIG_RIGHT, ECHO_RIGHT);
 
-  digitalWrite(trig,HIGH);
+  Serial.print("Front: ");
+  Serial.print(frontDistance);
+  Serial.print("  Left: ");
+  Serial.print(leftDistance);
+  Serial.print("  Right: ");
+  Serial.println(rightDistance);
+
+  // follows the left wall all the time
+
+  if(leftDistance > WALL_DISTANCE) 
+  {
+    turnLeft90();
+    moveForward();
+  }
+  else if(frontDistance > WALL_DISTANCE) 
+  {
+    moveForward();
+  }
+  else if(rightDistance > WALL_DISTANCE) 
+  {
+    turnRight90();
+    moveForward();
+  }
+  else 
+  {
+    // dead end
+    turnRight90();
+    turnRight90();
+  }
+
+}
+
+// functions for ultrasonic
+
+float getDistance(int trigPin, int echoPin) 
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+
+  digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trig,LOW);
 
-  long duration = pulseIn(echo,HIGH,ULTRASOUND_TIMEOUT_US);
+  digitalWrite(trigPin, LOW);
 
-  long distance = duration * 0.034 / 2;
+  long duration = pulseIn(echoPin, HIGH);
 
-  if(distance==0) distance = 100;
+  float distance = duration * 0.034 / 2;
 
   return distance;
 }
 
-// inset lorenzo line detection calibration here :3
+
+// movements
+
+void moveForward() 
+{
+  analogWrite(LEFT_FORWARD_PIN, motorSpeed);
+  analogWrite(LEFT_BACKWARD_PIN, 0);
+
+  analogWrite(RIGHT_FORWARD_PIN, motorSpeed);
+  analogWrite(RIGHT_BACKWARD_PIN, 0);
+
+  delay(350);
+
+  stopMotors();
+}
+
+void turnLeft90() 
+{
+  analogWrite(LEFT_FORWARD_PIN, 0);
+  analogWrite(LEFT_BACKWARD_PIN, motorSpeed);
+
+  analogWrite(RIGHT_FORWARD_PIN, motorSpeed);
+  analogWrite(RIGHT_BACKWARD_PIN, 0);
+
+  delay(330);
+
+  stopMotors();
+}
+
+void turnRight90()
+{
+  analogWrite(LEFT_FORWARD_PIN, motorSpeed);
+  analogWrite(LEFT_BACKWARD_PIN, 0);
+
+  analogWrite(RIGHT_FORWARD_PIN, 0);
+  analogWrite(RIGHT_BACKWARD_PIN, motorSpeed);
+
+  delay(330);
+
+  stopMotors();
+}
+
+void stopMotors()
+{
+  analogWrite(LEFT_FORWARD_PIN, 0);
+  analogWrite(LEFT_BACKWARD_PIN, 0);
+
+  analogWrite(RIGHT_FORWARD_PIN, 0);
+  analogWrite(RIGHT_BACKWARD_PIN, 0);
+}
