6109e012c55542aa935daaf74fee278f27128776
JP <jp.moro.machado@student.nhlstenden.com>
2026-03-12 17:34:56 +0100

    Merged version of Lorenzos Maze and game logic

--- Team 3/W5/Maze_logic_V1/Maze_logic_V1/Maze_logic_V1.ino
+++ Team 3/W5/Maze_logic_V1/Maze_logic_V1/Maze_logic_V1.ino
@@ -1,169 +1,338 @@
-// pins 
-const int LEFT_FORWARD_PIN  = 3;
+// Motor pins
+const int LEFT_FORWARD_PIN  = 6;
 const int LEFT_BACKWARD_PIN = 10;
 const int RIGHT_FORWARD_PIN = 11;
 const int RIGHT_BACKWARD_PIN = 9;
 
-int motorSpeed = 150;
+// Rotation pins
+const int ROTATION_LEFT_PIN  = 2;
+const int ROTATION_RIGHT_PIN = 3;
 
-// ultrasonic 
-// front
+// Ultrasonic pins
 const int TRIG_FRONT = 12;
 const int ECHO_FRONT = 13;
-// leftie
-const int TRIG_LEFT = 7;
-const int ECHO_LEFT = 4;
-// right
+
+const int TRIG_LEFT  = 7;
+const int ECHO_LEFT  = 4;
+
 const int TRIG_RIGHT = A3;
 const int ECHO_RIGHT = 8;
 
-// distance to wall
-const int WALL_DISTANCE = 20; // cm
-const int TOO_CLOSE_TO_WALLS = 8; // cm
+// Light sensors
+const int NUM_SENSORS = 4;
+
+const int LIGHT_SENSOR_PINS[NUM_SENSORS] = {A0, A1, A2, A4};
+
+const int BLACK_THRESHOLD = 600;
+
+// Gripper
+const int SERVO_PIN = 5;
+
+const int GRIPPER_OPEN_US  = 1820;
+const int GRIPPER_CLOSE_US = 1000;
+
+volatile int gripperPulseUs = GRIPPER_CLOSE_US;
+
+unsigned long lastServoMs = 0;
+
+// Maze settings
+const int WALL_DISTANCE = 20;
+
+// Encoder variables
+
+volatile unsigned long leftTickCount  = 0;
+volatile unsigned long rightTickCount = 0;
+
+// Wheel constants
+
+const float WHEEL_DIAMETER_CM      = 6.5;
+const float WHEEL_CIRCUMFERENCE_CM = PI * WHEEL_DIAMETER_CM;
+
+const float TICKS_PER_REVOLUTION = 40.0;
+const float TICKS_PER_CM         = TICKS_PER_REVOLUTION / WHEEL_CIRCUMFERENCE_CM;
 
 
-// setup
-void setup() {
+// Ultrasound constants
+// speed of sound / 2
 
-  Serial.begin(9600);
+const float SOUND_HALF = 0.017;
 
-  pinMode(LEFT_FORWARD_PIN, OUTPUT);
-  pinMode(LEFT_BACKWARD_PIN, OUTPUT);
-  pinMode(RIGHT_FORWARD_PIN, OUTPUT);
-  pinMode(RIGHT_BACKWARD_PIN, OUTPUT);
+// Setup
 
-  pinMode(TRIG_FRONT, OUTPUT);
-  pinMode(ECHO_FRONT, INPUT);
+void setup()
+{
+    Serial.begin(115200);
 
-  pinMode(TRIG_LEFT, OUTPUT);
-  pinMode(ECHO_LEFT, INPUT);
+    pinMode(LEFT_FORWARD_PIN, OUTPUT);
+    pinMode(LEFT_BACKWARD_PIN, OUTPUT);
+    pinMode(RIGHT_FORWARD_PIN, OUTPUT);
+    pinMode(RIGHT_BACKWARD_PIN, OUTPUT);
 
-  pinMode(TRIG_RIGHT, OUTPUT);
-  pinMode(ECHO_RIGHT, INPUT);
+    pinMode(TRIG_FRONT, OUTPUT);
+    pinMode(ECHO_FRONT, INPUT);
 
-  stopMotors();
+    pinMode(TRIG_LEFT, OUTPUT);
+    pinMode(ECHO_LEFT, INPUT);
+
+    pinMode(TRIG_RIGHT, OUTPUT);
+    pinMode(ECHO_RIGHT, INPUT);
+
+    for (int i = 0; i < NUM_SENSORS; i++)
+    {
+        pinMode(LIGHT_SENSOR_PINS[i], INPUT);
+    }
+
+    pinMode(SERVO_PIN, OUTPUT);
+
+    pinMode(ROTATION_LEFT_PIN, INPUT_PULLUP);
+    pinMode(ROTATION_RIGHT_PIN, INPUT_PULLUP);
+
+    attachInterrupt(
+        digitalPinToInterrupt(ROTATION_LEFT_PIN),
+        leftEncoderInterrupt,
+        CHANGE
+    );
+
+    attachInterrupt(
+        digitalPinToInterrupt(ROTATION_RIGHT_PIN),
+        rightEncoderInterrupt,
+        CHANGE
+    );
+
+    stopMotors();
 }
 
-// main loop
+// Main loop
 
 void loop() {
-
-  float frontDistance = getDistance(TRIG_FRONT, ECHO_FRONT);
-  float leftDistance  = getDistance(TRIG_LEFT, ECHO_LEFT);
-  float rightDistance = getDistance(TRIG_RIGHT, ECHO_RIGHT);
-
-  Serial.print("Front: ");
-  Serial.print(frontDistance);
-  Serial.print("  Left: ");
-  Serial.print(leftDistance);
-  Serial.print("  Right: ");
-  Serial.println(rightDistance);
-
-  // small adjustment if too close to wall
-  if(leftDistance < TOO_CLOSE_TO_WALLS) 
-  {
-    // turn slightly right
-    analogWrite(LEFT_FORWARD_PIN, motorSpeed);
-    analogWrite(RIGHT_FORWARD_PIN, 0);
-    analogWrite(RIGHT_BACKWARD_PIN, motorSpeed);
-    delay(80);
-    stopMotors();
-  }
   
-  if(rightDistance < TOO_CLOSE_TO_WALLS)
-  {
-  // turn slightly left
-    analogWrite(LEFT_BACKWARD_PIN, motorSpeed);
-    analogWrite(RIGHT_FORWARD_PIN, motorSpeed);
-    delay(80);
+    runCalibration();
+
+    followBlackLineUntilMaze();
+
+    solveMaze();
+
+    followBlackLineUntilEnd();
+
+    openGripper();
+
     stopMotors();
-  }
 
-  // follows the left wall all the time
-
-  if(leftDistance > WALL_DISTANCE) 
-  {
-    turnLeft90();
-    moveForward();
-  }
-  else if(frontDistance > WALL_DISTANCE) 
-  {
-    moveForward();
-  }
-  else if(rightDistance > WALL_DISTANCE) 
-  {
-    turnRight90();
-    moveForward();
-  }
-  else 
-  {
-    // dead end
-    turnRight90();
-    turnRight90();
-  }
-
+    while (true) {
+        // Stop forever
+    }
 }
 
-// functions for ultrasonic
-float getDistance(int trigPin, int echoPin) 
-{
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(50);
+// Line following
 
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(100);
+void followBlackLineUntilMaze() {
+    while (true) {
+        int left  = analogRead(LIGHT_SENSOR_PINS[0]);
+        int midL  = analogRead(LIGHT_SENSOR_PINS[1]);
+        int midR  = analogRead(LIGHT_SENSOR_PINS[2]);
+        int right = analogRead(LIGHT_SENSOR_PINS[3]);
 
-  digitalWrite(trigPin, LOW);
+        float front = getDistance(TRIG_FRONT, ECHO_FRONT);
 
-  long duration = pulseIn(echoPin, HIGH);
-// this is speed of sound by microseconds so: distance (cm) = duration (µs/microseconds)* 0.017
-// 0.017 is speed of sound already divided by 2, so instead of 0.034 its 0.017
-  float distance = duration * 0.017; 
+        if (front > 30) {
+            stopMotors();
+            return;
+        }
 
-  return distance;
+        if (midL > BLACK_THRESHOLD || midR > BLACK_THRESHOLD) {
+            moveForwardCm(4);
+        }
+        else if (left > BLACK_THRESHOLD) {
+            turnLeftSmall();
+        }
+        else if (right > BLACK_THRESHOLD) {
+            turnRightSmall();
+        }
+        else {
+            moveForwardCm(2);
+        }
+    }
 }
 
-// movements
-void moveForward() {
-  analogWrite(LEFT_FORWARD_PIN, motorSpeed);
-  analogWrite(LEFT_BACKWARD_PIN, 0);
 
-  analogWrite(RIGHT_FORWARD_PIN, motorSpeed);
-  analogWrite(RIGHT_BACKWARD_PIN, 0);
+void followBlackLineUntilEnd() {
+    while (true) {
+        if (allSensorsBlack()) {
+            stopMotors();
+            return;
+        }
 
-  delay(400);
+        int left  = analogRead(LIGHT_SENSOR_PINS[0]);
+        int midL  = analogRead(LIGHT_SENSOR_PINS[1]);
+        int midR  = analogRead(LIGHT_SENSOR_PINS[2]);
+        int right = analogRead(LIGHT_SENSOR_PINS[3]);
 
-  stopMotors();
+        if (midL > BLACK_THRESHOLD || midR > BLACK_THRESHOLD) {
+            moveForwardCm(4);
+        }
+        else if (left > BLACK_THRESHOLD) {
+            turnLeftSmall();
+        }
+        else if (right > BLACK_THRESHOLD) {
+            turnRightSmall();
+        }
+        else {
+            moveForwardCm(2);
+        }
+    }
 }
+
+// MAZE SOLVER
+
+void solveMaze() {
+    while (true) {
+        if (checkBlackLine()) {
+            stopMotors();
+            return;
+        }
+
+        float front = getDistance(TRIG_FRONT, ECHO_FRONT);
+        float left  = getDistance(TRIG_LEFT, ECHO_LEFT);
+        float right = getDistance(TRIG_RIGHT, ECHO_RIGHT);
+
+        if (left > WALL_DISTANCE) {
+            turnLeft90();
+            moveForwardCm(25);
+        }
+        else if (front > WALL_DISTANCE) {
+            moveForwardCm(25);
+        }
+        else if (right > WALL_DISTANCE) {
+            turnRight90();
+            moveForwardCm(25);
+        }
+        else {
+            turnRight90();
+            turnRight90();
+        }
+    }
+}
+
+// Ultrasonic
+
+float getDistance(int trigPin, int echoPin) {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+
+    digitalWrite(trigPin, LOW);
+
+    long duration = pulseIn(echoPin, HIGH);
+
+    return duration * SOUND_HALF;
+}
+
+// Movement
+
+void moveForwardCm(float cm) {
+    int target = cm * TICKS_PER_CM;
+
+    resetEncoders();
+
+    analogWrite(LEFT_FORWARD_PIN, 200);
+    analogWrite(RIGHT_FORWARD_PIN, 200);
+
+    while ((leftTickCount + rightTickCount) / 2 < target) {
+        gripperUpdate();
+    }
+
+    stopMotors();
+}
+
 
 void turnLeft90() {
-  analogWrite(LEFT_FORWARD_PIN, 0);
-  analogWrite(LEFT_BACKWARD_PIN, motorSpeed);
-
-  analogWrite(RIGHT_FORWARD_PIN, motorSpeed);
-  analogWrite(RIGHT_BACKWARD_PIN, 0);
-
-  delay(330);
-
-  stopMotors();
+    analogWrite(RIGHT_FORWARD_PIN, 200);
+    delay(330);
+    stopMotors();
 }
 
-void turnRight90(){
-  analogWrite(LEFT_FORWARD_PIN, motorSpeed);
-  analogWrite(LEFT_BACKWARD_PIN, 0);
-
-  analogWrite(RIGHT_FORWARD_PIN, 0);
-  analogWrite(RIGHT_BACKWARD_PIN, motorSpeed);
-
-  delay(330);
-
-  stopMotors();
+void turnRight90() {
+    analogWrite(LEFT_FORWARD_PIN, 200);
+    delay(330);
+    stopMotors();
 }
 
-void stopMotors(){
-  analogWrite(LEFT_FORWARD_PIN, 0);
-  analogWrite(LEFT_BACKWARD_PIN, 0);
+void turnLeftSmall() {
+    analogWrite(RIGHT_FORWARD_PIN, 200);
+    delay(60);
+    stopMotors();
+}
 
-  analogWrite(RIGHT_FORWARD_PIN, 0);
-  analogWrite(RIGHT_BACKWARD_PIN, 0);
+void turnRightSmall() {
+    analogWrite(LEFT_FORWARD_PIN, 200);
+    delay(60);
+    stopMotors();
+}
+
+void stopMotors() {
+    analogWrite(LEFT_FORWARD_PIN, 0);
+    analogWrite(LEFT_BACKWARD_PIN, 0);
+    analogWrite(RIGHT_FORWARD_PIN, 0);
+    analogWrite(RIGHT_BACKWARD_PIN, 0);
+}
+
+// Encoders
+
+void leftEncoderInterrupt() {
+    leftTickCount++;
+}
+
+void rightEncoderInterrupt() {
+    rightTickCount++;
+}
+
+void resetEncoders() {
+    leftTickCount  = 0;
+    rightTickCount = 0;
+}
+
+// Light sensor helpers
+
+bool allSensorsBlack() {
+    for (int i = 0; i < NUM_SENSORS; i++) {
+        if (analogRead(LIGHT_SENSOR_PINS[i]) < BLACK_THRESHOLD) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+bool checkBlackLine() {
+    for (int i = 0; i < NUM_SENSORS; i++) {
+        if (analogRead(LIGHT_SENSOR_PINS[i]) > BLACK_THRESHOLD) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Calibration
+
+void runCalibration() {
+    delay(2000);
+}
+
+// Gripper
+
+void gripperUpdate() {
+    unsigned long now = millis();
+    if (now - lastServoMs >= 20) {
+        lastServoMs = now;
+
+        digitalWrite(SERVO_PIN, HIGH);
+        delayMicroseconds(gripperPulseUs);
+        digitalWrite(SERVO_PIN, LOW);
+    }
+}
+
+void openGripper() {
+    gripperPulseUs = GRIPPER_OPEN_US;
 }
